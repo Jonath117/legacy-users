2d470b0aa51789c0d74b4732ea599ea4e0dfe2b4
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-20 19:52:09 -0400

    feat: restriccion de IP y escalado a t3.micro por seguridad

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "app_port" {
 variable "instance_type" {
   description = "Tipo de instancia EC2"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro" 
 }
 
 variable "iam_instance_profile" {
